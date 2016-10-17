c9ab97c9ab018a329eced26f5aa0ea9ee9715e08
ryjaled <ryjaled@gmail.com>
2016-10-17 14:08:49 +0000

    comitttt
    commit

--- scripts/index.js
+++ scripts/index.js
@@ -37,6 +37,8 @@
 	}
 
 
+//barcode scanner
+
 	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 	function onDeviceReady() {
 
@@ -66,6 +68,36 @@
 
 	}
 	};
+
+
+
+
+	document.addEventListener("deviceready", onDeviceReady, false);
+	    function onDeviceReady() {
+					document.getElementById("gelocator").onclick = function(){
+
+					navigator.geolocation.getCurrentPosition(onSuccess, onError);
+	        console.log("navigator.geolocation works well");
+	    }
+		}
+
+			var onSuccess = function(position) {
+						 alert('Latitude: '          + position.coords.latitude          + '\n' +
+									 'Longitude: '         + position.coords.longitude         + '\n' +
+									 'Altitude: '          + position.coords.altitude          + '\n' +
+									 'Accuracy: '          + position.coords.accuracy          + '\n' +
+									 'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+									 'Heading: '           + position.coords.heading           + '\n' +
+									 'Speed: '             + position.coords.speed             + '\n' +
+									 'Timestamp: '         + position.timestamp                + '\n');
+				 };
+
+				 // onError Callback receives a PositionError object
+				 //
+				 function onError(error) {
+						 alert('code: '    + error.code    + '\n' +
+									 'message: ' + error.message + '\n');
+				 }
 
 
 
